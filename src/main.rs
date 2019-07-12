7d7ed3f1b29069d60f54235dbb30dbc58ae2b511
Beat Wolf <beat.wolf@hefr.ch>
2019-07-12 17:05:27 +0200

    List wifis and start of interface

--- src/main.rs
+++ src/main.rs
@@ -10,37 +10,49 @@ use rocket_contrib::json::Json;
 use rocket::http::RawStr;
 
 use serde::Serialize;
-
+use std::collections::HashMap;
+use std::ops::Deref;
 
 #[get("/wifis")]
-fn index() -> Json<Vec<Wifi>> {
+fn index() -> Json<Vec<CustomWifi>> {
 
     let mut echo_hello = Command::new("nmcli");
-    echo_hello.arg("-c").arg("no").arg("dev").arg("wifi");
+    echo_hello.arg("-c").arg("no").arg("--terse").arg("dev").arg("wifi");
     let hello_1 = echo_hello.output().expect("failed to execute process");
 
     let output = String::from_utf8_lossy(&hello_1.stdout);
 
-    let mut lines : Vec<Wifi> = Vec::new();
+    let mut wifis = HashMap::new();
 
     for line in output.as_ref().lines() {
         //lines.push(String::from(line));
 
         if !line.starts_with("IN-USE") {
-            let split_line = line.split_ascii_whitespace().collect::<Vec<&str>>();
+            let split_line = line.split(":").collect::<Vec<&str>>();
 
-            if split_line.len() > 6 {
-                lines.push(Wifi{ssid: String::from("SSID"), speed : String::from("1234"), security: String::from("WPA")});
+            /*for i in 0..split_line.len(){
+                print!("{} {} ", i, split_line[i]);
+            }
+            println!();*/
+
+            if split_line.len() > 6 && split_line[1].len() > 0{
+                wifis.insert(String::from(split_line[1]), CustomWifi{ssid: String::from(split_line[1]), speed : String::from(split_line[4]), security: String::from(split_line[7])});
             }
         }
 
     }
 
-    return Json(lines);
+    let mut wifi_vec = Vec::new();
+
+    for(_, val) in wifis.iter(){
+        wifi_vec.push(val.clone());
+    }
+
+    return Json(wifi_vec);
 }
 
-#[derive(Serialize)]
-pub struct Wifi {
+#[derive(Serialize, Clone)]
+pub struct CustomWifi {
     pub ssid : String,
     pub speed : String,
     pub security : String
